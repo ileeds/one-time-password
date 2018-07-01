6dfb234f562ad0bc5456148eeadb97b96556d27e
ileeds <ileeds@brandeis.edu>
2018-07-01 18:42:10 -0400

    new user one time password

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,9 @@
 const admin = require('firebase-admin');
 const functions = require('firebase-functions');
-const createUser = require('./create_user');
+// const createUser = require('./create_user');
 const serviceAccount = require('./service_account.json');
-const requestOneTimePassword = require('./request_one_time_password');
+// const requestOneTimePassword = require('./request_one_time_password');
+const newUserOneTimePassword = require('./new_user_one_time_password');
 const verifyOneTimePassword = require('./verify_one_time_password');
 
 admin.initializeApp({
@@ -10,6 +11,7 @@ admin.initializeApp({
   databaseURL: "https://one-time-password-1bd5c.firebaseio.com"
 });
 
-exports.createUser = functions.https.onRequest(createUser);
-exports.requestOneTimePassword = functions.https.onRequest(requestOneTimePassword);
+exports.newUserOneTimePassword = functions.https.onRequest(newUserOneTimePassword);
+// exports.createUser = functions.https.onRequest(createUser);
+// exports.requestOneTimePassword = functions.https.onRequest(requestOneTimePassword);
 exports.verifyOneTimePassword = functions.https.onRequest(verifyOneTimePassword);
